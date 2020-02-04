56fcf75437c3073611fe31533ed30e431de7ed78
inboxpraveen <inbox.praveen.kumar17@gmail.com>
2020-02-04 15:01:42 +0530

    updated wc

---
+++ wordCountProject/wordCount/check.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+def home(request):
+    return render(request,'home.html')
+
+def count(request):
+    # txt = request.FILES['input_text']
+    if request.method == "POST":
+        uploaded_file = request.FILES['input_file']
+        file_name = uploaded_file.name
+        file_size = uploaded_file.size
+        text = []
+        # with open(uploaded_file,'rb') as main_file:
+        #     for word in main_file.red():
+        #         text.append(word)
+        # length = len(text)
+        text = uploaded_file.read().decode('utf-8')
+        new_text = []
+        for word in text.split(' '):
+            new_text.append(word)
+        length = len(new_text)
+    return render(request,'count.html',\
+    {
+    'FileName':file_name,
+    'FileSize':file_size,
+    'Text':text,
+    'Length':length
+    })
